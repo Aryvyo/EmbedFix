1912630e97c20cc80fd884e209366ccee1c64d6b
Lux <32790578+Luxetrixx@users.noreply.github.com>
2023-11-23 21:03:46 +0300

    added switch for other sites

--- content/content.js
+++ content/content.js
@@ -1,31 +1,30 @@
 console.log("content.js loaded");
   
 if(document.readyState !== 'complete') {
-    console.log("window not loaded");
     window.addEventListener('load',() => {console.log("window loaded");afterWindowLoaded();});
 } else {
-    console.log("window loaded");
     afterWindowLoaded();
 }
 
-function isXShareButton(element) {
-    return (
-      element &&
-      element.tagName.toLowerCase() === 'div' &&
-      element.classList.contains('css-175oi2r r-1loqt21 r-18u37iz r-ymttw5 r-1f1sjgu r-13qz1uu r-o7ynqc r-6416eg r-1ny4l3l')
-
-    );
-  }
   
   function afterWindowLoaded () {
-    console.log("after window loaded");
-    document.addEventListener('copy', (event) => {
+    document.addEventListener('copy', async (event) => {
         console.log(event.target.textContent);
-        newUrl = event.target.textContent.replace("x.com", "fixupx.com");
-        console.log(newUrl);
-        navigator.clipboard.writeText(newUrl);
+        let text = event.target.textContent;
 
+
+        let url = text.split("/")[2];
+
+        let switchUrl = links[url];
+
+        let newText = text.replace(url, switchUrl);
+
+        navigator.clipboard.writeText(newText)
     })
-    console.log("after listener added loaded");
 };
 
+const links = {
+    "x.com":"fixupx.com",
+    "instagram.com":"ddinstagram.com"
+  }
+  
