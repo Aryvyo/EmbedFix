13fb5d08a9c3515de030d2cd4e1209a5fb8f7de5
Lux <32790578+Luxetrixx@users.noreply.github.com>
2023-11-25 12:46:23 +0300

    fixed link grabbing on sites that do not use textContent removed useless permissions fixed content_script matches

--- content/content.js
+++ content/content.js
@@ -3,29 +3,56 @@ console.log("content.js loaded");
 if(document.readyState !== 'complete') {
     window.addEventListener('load',() => {console.log("window loaded");afterWindowLoaded();});
 } else {
+    
     afterWindowLoaded();
 }
 
   
   function afterWindowLoaded () {
+    console.log("afterWindowLoaded");
     document.addEventListener('copy', async (event) => {
-        console.log(event.target.textContent);
-        let text = event.target.textContent;
+        console.log(event)
+        console.log("content: " + event.target.textContent);
+        console.log("baseURI: " + event.target.baseURI);
 
+        if(event.target.textContent == ""){
+            let text = ""
+            if(event.target.value == undefined){
+                text = event.target.baseURI;
+            }
+            else{
+                text = event.target.value;
+            }
+            let newText = replaceLink(text);
+            navigator.clipboard.writeText(newText)
+            return;
+        }
+        else{
+            let text = event.target.textContent;
+            let newText = replaceLink(text);
+            navigator.clipboard.writeText(newText)
+            return;
+        }
 
-        let url = text.split("/")[2];
-
-        let switchUrl = links[url];
-
-        let newText = text.replace(url, switchUrl);
-
-        navigator.clipboard.writeText(newText)
     })
 };
+
+function replaceLink(text){
+    let url = text.split("/")[2];
+
+    let switchUrl = links[url];
+
+    let newText = text.replace(url, switchUrl);
+
+    return newText;
+}
 
 const links = {
     "x.com":"fixupx.com",
     "instagram.com":"ddinstagram.com",
     "www.instagram.com":"www.ddinstagram.com",
+    "www.tiktok.com":"www.tiktxk.com",
+    "tiktok.com":"tiktxk.com",
+    "twitter.com":"vxtwitter.com",
   }
   
